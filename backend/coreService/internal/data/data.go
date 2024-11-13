bbcdd09476caee65c7cf2c26ae0d63a9d86430b3
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-11-14 00:03:12 +0800

    feat: gorm gen model and basic query and add user Usecase Repo

--- backend/coreService/internal/data/data.go
+++ backend/coreService/internal/data/data.go
@@ -2,9 +2,14 @@ package data
 
 import (
 	"coreService/internal/conf"
+	"fmt"
+	"os"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // ProviderSet is data providers.
@@ -21,4 +26,22 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 	return &Data{}, cleanup, nil
+}
+
+func NewOrmDatabase(c *conf.Data) *gorm.DB {
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DB"),
+	)
+	log.Info("dns: ", dns)
+	db, err := gorm.Open(mysql.Open(dns), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	}
+	return db
 }
---
+++ backend/coreService/internal/data/gen/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"coreService/internal/data"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gen"
+)
+
+// generate model and basic query function
+func main() {
+	godotenv.Load()
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/data/query", // output path
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	gormdb := data.NewOrmDatabase(nil)
+	g.UseDB(gormdb) // reuse your gorm db
+
+	// Generate basic type-safe DAO API for struct `model.User` following conventions
+
+	// g.ApplyBasic(
+	// 	// Generate struct `User` based on table `users`
+	// 	g.GenerateModel("users"),
+
+	// 	// Generate struct `Employee` based on table `users`
+	// 	g.GenerateModelAs("users", "Employee"),
+
+	// 	// Generate struct `User` based on table `users` and generating options
+	// 	g.GenerateModel("users", gen.FieldIgnore("address"), gen.FieldType("id", "int64")),
+
+	// 	// Generate struct `Customer` based on table `customer` and generating options
+	// 	// customer table may have a tags column, it can be JSON type, gorm/gen tool can generate for your JSON data type
+	// 	g.GenerateModel("customer", gen.FieldType("tags", "datatypes.JSON")),
+	// )
+	g.ApplyBasic(
+		// Generate structs from all tables of current database
+		g.GenerateAllTable()...,
+	)
+	// Generate the code
+	g.Execute()
+}
