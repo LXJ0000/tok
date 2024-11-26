e8d29b0fc28c4643234bb31fb11e8c281670cb9d
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-11-26 11:57:16 +0800

    feat(backend.basicService): account service

--- backend/basicService/internal/service/accountservice.go
+++ backend/basicService/internal/service/accountservice.go
@@ -2,27 +2,66 @@ package service
 
 import (
 	"context"
+	"errors"
 
 	pb "github.com/LXJ0000/tok/backend/basicService/api/basicService/v1"
+	"github.com/LXJ0000/tok/backend/basicService/internal/biz"
 )
 
 type AccountServiceService struct {
 	pb.UnimplementedAccountServiceServer
+	accountUc *biz.AccountUsecase
 }
 
-func NewAccountServiceService() *AccountServiceService {
-	return &AccountServiceService{}
+func NewAccountServiceService(accountUc *biz.AccountUsecase) *AccountServiceService {
+	return &AccountServiceService{accountUc: accountUc}
 }
 
 func (s *AccountServiceService) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	return &pb.RegisterResponse{}, nil
+	a := &biz.Account{
+		Mobile:   req.Mobile,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+	if err := s.accountUc.CreateAccount(ctx, a); err != nil {
+		return nil, err
+	}
+	return &pb.RegisterResponse{
+		Meta:      SuccessMeta,
+		AccountId: a.ID,
+	}, nil
 }
+
 func (s *AccountServiceService) CheckAccount(ctx context.Context, req *pb.CheckAccountRequest) (*pb.CheckAccountResponse, error) {
-	return &pb.CheckAccountResponse{}, nil
+	var (
+		id  int64
+		err error
+	)
+	if req.AccountId != 0 {
+		if id, err = s.accountUc.CheckPasswordByID(ctx, req.AccountId, req.Password); err != nil {
+			return nil, err
+		}
+	} else if req.Mobile != "" {
+		if id, err = s.accountUc.CheckPasswordByMobile(ctx, req.Mobile, req.Password); err != nil {
+			return nil, err
+		}
+	} else if req.Email != "" {
+		if id, err = s.accountUc.CheckPasswordByEmail(ctx, req.Email, req.Password); err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, errors.New("invalid request")
+	}
+	return &pb.CheckAccountResponse{
+		Meta:      SuccessMeta,
+		AccountId: id,
+	}, nil
 }
+
 func (s *AccountServiceService) Bind(ctx context.Context, req *pb.BindRequest) (*pb.BindResponse, error) {
 	return &pb.BindResponse{}, nil
 }
+
 func (s *AccountServiceService) Unbind(ctx context.Context, req *pb.UnbindRequest) (*pb.UnbindResponse, error) {
 	return &pb.UnbindResponse{}, nil
 }
--- backend/basicService/internal/service/service.go
+++ backend/basicService/internal/service/service.go
@@ -1,6 +1,18 @@
 package service
 
-import "github.com/google/wire"
+import (
+	"net/http"
+
+	pb "github.com/LXJ0000/tok/backend/basicService/api/basicService/v1"
+	"github.com/google/wire"
+)
 
 // ProviderSet is service providers.
 var ProviderSet = wire.NewSet(NewGreeterService)
+
+var (
+	SuccessMeta = &pb.Metadata{
+		BizCode: http.StatusOK,
+		Message: "success",
+	}
+)
