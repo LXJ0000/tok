f9574b0e9a7f8f6e35d2a37231255ff10e3e956b
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-11-27 10:40:18 +0800

    feat: 表结构变更

--- script/gen_sql/20241113064317_v1_0_0.sql
+++ script/gen_sql/20241113064317_v1_0_0.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS account
     password    VARCHAR(64)  NOT NULL COMMENT '密码',
     salt        VARCHAR(64)  NOT NULL COMMENT '密码盐',
     number      VARCHAR(15)  NOT NULL DEFAULT '' COMMENT '号码',
-    is_deleted  BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted  TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     INDEX account_mobile_idx (mobile) COMMENT '手机号索引',
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS user
     avatar           VARCHAR(255) NOT NULL DEFAULT '' COMMENT '头像',
     background_image VARCHAR(255) NOT NULL DEFAULT '' COMMENT '背景图片',
     signature        VARCHAR(255) NOT NULL DEFAULT '' COMMENT '签名',
-    is_deleted       BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted       TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     UNIQUE INDEX idx_users_account_id (account_id) USING BTREE COMMENT '账户ID唯一索引'
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS template
     id          BIGINT PRIMARY KEY COMMENT '模板ID',
     title       VARCHAR(255) NOT NULL COMMENT '标题',
     content     TEXT         NOT NULL COMMENT '内容',
-    is_deleted  BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted  TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     INDEX create_time_idx (create_time) COMMENT '创建时间索引',
@@ -60,8 +60,8 @@ CREATE TABLE IF NOT EXISTS file
     hash        VARCHAR(255) NOT NULL COMMENT '文件哈希',
     file_size   BIGINT       NOT NULL DEFAULT 0 COMMENT '文件大小',
     file_type   VARCHAR(255) NOT NULL COMMENT '文件类型',
-    uploaded    BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否上传',
-    is_deleted  BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    uploaded    TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否上传',
+    is_deleted  TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     INDEX create_time_idx (create_time) COMMENT '创建时间索引',
@@ -83,7 +83,7 @@ CREATE TABLE IF NOT EXISTS video
     collect_count BIGINT       NOT NULL DEFAULT 0 COMMENT '收藏数',
     forward_count BIGINT       NOT NULL DEFAULT 0 COMMENT '转发数',
     comment_count BIGINT       NOT NULL DEFAULT 0 COMMENT '评论数',
-    is_deleted    BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted    TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 );
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS comment
     to_user_id     BIGINT                DEFAULT NULL COMMENT '评论所回复的用户ID',
     content        VARCHAR(512) NOT NULL COMMENT '评论内容',
     first_comments JSON         NOT NULL COMMENT '最开始的几条子评论',
-    is_deleted     BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted     TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     INDEX video_id_idx (video_id, is_deleted) COMMENT '视频ID索引',
@@ -116,7 +116,7 @@ CREATE TABLE IF NOT EXISTS collection
     user_id     BIGINT       NOT NULL COMMENT '用户ID',
     title       VARCHAR(255) NOT NULL COMMENT '标题',
     description TEXT         NOT NULL COMMENT '描述',
-    is_deleted  BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted  TINYINT      NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     INDEX user_id_idx (user_id, is_deleted) COMMENT '用户ID索引',
@@ -131,10 +131,62 @@ CREATE TABLE IF NOT EXISTS follow
     id             BIGINT PRIMARY KEY COMMENT '关注ID',
     user_id        BIGINT    NOT NULL COMMENT '用户ID',
     target_user_id BIGINT    NOT NULL COMMENT '被关注的用户ID',
-    is_deleted     BOOLEAN   NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    is_deleted     TINYINT   NOT NULL DEFAULT FALSE COMMENT '是否删除',
     create_time    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     update_time    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    INDEX user_id_idx (user_id, target_user_id, is_deleted) COMMENT '用户ID索引'
+    INDEX user_id_idx (
+                       user_id,
+                       target_user_id,
+                       is_deleted
+        ) COMMENT '用户ID索引'
+);
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS interaction
+(
+    id          BIGINT PRIMARY KEY,
+    biz_id      BIGINT             DEFAULT NULL,
+    biz         varchar(255)       DEFAULT NULL,
+    read_cnt    BIGINT    NOT NULL DEFAULT 0,
+    like_cnt    BIGINT    NOT NULL DEFAULT 0,
+    collect_cnt BIGINT    NOT NULL DEFAULT 0,
+    is_deleted  TINYINT   NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    UNIQUE KEY idx_interaction_bizID_biz (biz_id, biz)
+);
+-- +goose StatementEnd
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS user_collect
+(
+    id            BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    user_id       BIGINT                   DEFAULT NULL,
+    biz_id        BIGINT                   DEFAULT NULL,
+    biz           VARCHAR(255)             DEFAULT NULL,
+    collection_id BIGINT                   DEFAULT NULL,
+    status        TINYINT(1)      NOT NULL DEFAULT 0 COMMENT '0=不收藏 1=收藏',
+    is_deleted    TINYINT         NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    create_time   TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time   TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (id),
+    UNIQUE KEY idx_userCollect_userID_bizID_biz (user_id, biz_id, biz),
+    KEY idx_user_collect_collection_id (collection_id)
+);
+-- +goose StatementEnd
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS user_like
+(
+    id          BIGINT unsigned NOT NULL AUTO_INCREMENT,
+    user_id     BIGINT                   DEFAULT NULL,
+    biz_id      BIGINT                   DEFAULT NULL,
+    biz         VARCHAR(255)             DEFAULT NULL,
+    status      TINYINT(1)      NOT NULL DEFAULT 0 COMMENT '0=不点赞 1=点赞',
+    is_deleted  TINYINT         NOT NULL DEFAULT FALSE COMMENT '是否删除',
+    create_time TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    update_time TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    PRIMARY KEY (id),
+    UNIQUE KEY idx_userLike_userID_bizID_biz (user_id, biz_id, biz)
 );
 -- +goose StatementEnd
 
@@ -162,4 +214,13 @@ DROP TABLE IF EXISTS collection;
 -- +goose StatementEnd
 -- +goose StatementBegin
 DROP TABLE IF EXISTS follow;
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE `interaction`;
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE `user_collect`;
+-- +goose StatementEnd
+-- +goose StatementBegin
+DROP TABLE `user_like`;
 -- +goose StatementEnd
